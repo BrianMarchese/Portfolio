b5caecc61a9732442d973772e307672365c7c748
BrianMarchese <brianmarchese@hotmail.com>
2026-05-03 20:20:46 -0300

    solucion de fecha incorrecta en formacion y actualizacion de CV

--- src/components/lineaTiempo/LineaTiempo.tsx
+++ src/components/lineaTiempo/LineaTiempo.tsx
@@ -19,7 +19,7 @@ const educationData: EducationItem[] = [
     descripcion: "Formación universitaria enfocada principalmente en los cimientos de la ingeniería de software y las ciencias de la computación."
   },
   {
-    fecha: "2026 - 2020",
+    fecha: "2016 - 2021",
     titulo: "Bachiller en Economía y Administración",
     institucion: "Colegio Nuestra Señora de la Merced"
   }
